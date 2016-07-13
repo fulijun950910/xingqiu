149702bb078ed546111ebb6f04db6b711153fa4e
zhangzhen <zhen.zhang@mei1.com>
2016-07-13 14:41:23 +0800

    修复弹窗bug

--- src/js/app/app.home.js
+++ src/js/app/app.home.js
@@ -43,20 +43,19 @@ app.endLoading=function(){
 app.alert=function(msg,title){
 	var msg = msg || '';
 	var title = title || '提示';
-	if(!($('#dialog2').length>0)){
-		var altHtml = '';
-			altHtml+= '<div class="weui_dialog_alert" id="dialog2" style="display: none;">';
-			altHtml+= '<div class="weui_mask"></div>';
-			altHtml+= '<div class="weui_dialog">';
-			altHtml+= '<div class="weui_dialog_hd"><strong class="weui_dialog_title">'+title+'</strong></div>';
-			altHtml+= '<div class="weui_dialog_bd">'+msg+'</div>';
-			altHtml+= '<div class="weui_dialog_ft">';
-			altHtml+= '<a href="javascript:;" class="weui_btn_dialog primary">确定</a>';
-			altHtml+= '</div></div></div>';
-		$('body').append(altHtml);
-	}
+	var altHtml = '';
+		altHtml+= '<div class="weui_dialog_alert" id="dialog2" style="display: none;">';
+		altHtml+= '<div class="weui_mask"></div>';
+		altHtml+= '<div class="weui_dialog">';
+		altHtml+= '<div class="weui_dialog_hd"><strong class="weui_dialog_title">'+title+'</strong></div>';
+		altHtml+= '<div class="weui_dialog_bd">'+msg+'</div>';
+		altHtml+= '<div class="weui_dialog_ft">';
+		altHtml+= '<a href="javascript:;" class="weui_btn_dialog primary">确定</a>';
+		altHtml+= '</div></div></div>';
+	$('body').append(altHtml);
 	$('#dialog2').show().on('click', '.weui_btn_dialog', function () {
         $('#dialog2').off('click').hide();
+       	$('#dialog2').remove();
         return true;
     });
 }
@@ -64,20 +63,19 @@ app.alert=function(msg,title){
 //微信操作完成透明提示窗口
 app.toast=function(msg){
 	var msg = msg || '已完成';
-	if(!($('#toast').length>0)){
-		var toastHtml = '';
-			toastHtml+= '<div id="toast" style="display: none;">';
-			toastHtml+= '<div class="weui_mask_transparent"></div>';
-			toastHtml+= '<div class="weui_toast">';
-			toastHtml+= '<i class="weui_icon_toast"></i>';
-			toastHtml+= '<p class="weui_toast_content">'+msg+'</p>';
-	    	toastHtml+= '</div>';
-	    	toastHtml+= '</div>';
-    	$('body').append(toastHtml);
-	}
+	var toastHtml = '';
+		toastHtml+= '<div id="toast" style="display: none;">';
+		toastHtml+= '<div class="weui_mask_transparent"></div>';
+		toastHtml+= '<div class="weui_toast">';
+		toastHtml+= '<i class="weui_icon_toast"></i>';
+		toastHtml+= '<p class="weui_toast_content">'+msg+'</p>';
+    	toastHtml+= '</div>';
+    	toastHtml+= '</div>';
+	$('body').append(toastHtml);
 	$('#toast').show();
 	setTimeout(function () {
         $('#toast').hide();
+        $('#toast').remove();
     }, 2000);
 }
 
@@ -85,21 +83,20 @@ app.toast=function(msg){
 app.confirm=function(msg,title){
 	var msg = msg || '';
 	var title = title || '提示';
-	if(!($('#dialog1').length>0)){
-		var confirmHtml = '';
-			confirmHtml+= '<div class="weui_dialog_confirm" id="dialog1" style="display: none;">';
-			confirmHtml+= '<div class="weui_mask"></div>';
-			confirmHtml+= '<div class="weui_dialog">';
-			confirmHtml+= '<div class="weui_dialog_hd"><strong class="weui_dialog_title">'+title+'</strong></div>';
-			confirmHtml+= '<div class="weui_dialog_bd">'+msg+'</div>';
-			confirmHtml+= '<div class="weui_dialog_ft">';
-			confirmHtml+= '<a href="javascript:;" class="weui_btn_dialog default">取消</a>';
-			confirmHtml+= '<a href="javascript:;" class="weui_btn_dialog primary">确定</a>';
-			confirmHtml+= '</div></div></div>';
-		$('body').append(confirmHtml);
-	}
+	var confirmHtml = '';
+		confirmHtml+= '<div class="weui_dialog_confirm" id="dialog1" style="display: none;">';
+		confirmHtml+= '<div class="weui_mask"></div>';
+		confirmHtml+= '<div class="weui_dialog">';
+		confirmHtml+= '<div class="weui_dialog_hd"><strong class="weui_dialog_title">'+title+'</strong></div>';
+		confirmHtml+= '<div class="weui_dialog_bd">'+msg+'</div>';
+		confirmHtml+= '<div class="weui_dialog_ft">';
+		confirmHtml+= '<a href="javascript:;" class="weui_btn_dialog default">取消</a>';
+		confirmHtml+= '<a href="javascript:;" class="weui_btn_dialog primary">确定</a>';
+		confirmHtml+= '</div></div></div>';
+	$('body').append(confirmHtml);
 	$('#dialog1').show().on('click', '.weui_btn_dialog', function () {
         $('#dialog1').off('click').hide();
+        $('#dialog1').remove();
         return true;
     });
 }
