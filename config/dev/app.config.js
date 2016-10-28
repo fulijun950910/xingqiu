6fbbf932cd7a5b3137a813f5887094b16b775935
panxu <pan.xu@mei1.com>
2016-10-28 15:44:32 +0800

    icon更新

--- config/dev/app.config.js
+++ config/dev/app.config.js
@@ -1,6 +1,7 @@
 /**
  * Created by wzc on 16/8/2.
  */
-//app.url = 'http://localhost:8080';
-app.url = 'http://192.168.27.118:8069';
-app.filePath = 'http://localhost:8090/api/file/';
+//app.url = 'http://assistant.mei1.info';
+//app.url = 'http://192.168.27.161:8080';
+app.url = 'http://192.168.27.199:8069';
+app.filePath = 'http://localhost:8080/api/file/';
