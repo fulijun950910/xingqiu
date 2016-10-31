697983d027108b35a25dbeae733d8bf0a7d0907a
meixing <meixing.ruan@mei1.com>
2016-10-31 13:43:18 +0800

    签到

--- config/dev/app.config.js
+++ config/dev/app.config.js
@@ -2,5 +2,8 @@
  * Created by wzc on 16/8/2.
  */
 //app.url = 'http://localhost:8080';
+//app.url = 'http://192.168.27.161:8080';
+//app.url = 'http://assistant.mei1.info';
+//app.url = 'http://192.168.27.218:8080'
 app.url = 'http://192.168.27.118:8069';
-app.filePath = 'http://localhost:8090/api/file/';
+app.filePath = 'http://assistant.mei1.info/api/file/';
