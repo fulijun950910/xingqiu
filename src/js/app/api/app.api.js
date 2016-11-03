5e460e7f4560a86c227a6fcbf7675ab29baf4ac3
zhangzhen <zhen.zhang@mei1.com>
2016-11-03 14:33:25 +0800

    店务助手切换身份

--- src/js/app/api/app.api.js
+++ src/js/app/api/app.api.js
@@ -9,8 +9,7 @@ app.api = {
             type: options.type,
             headers: {
                 'Accept': '*/*',
-                'Content-Type': 'application/json',
-                'wechat': 'assistant'
+                'Content-Type': 'application/json'
             },
             dataType: 'json',
             success: options.success,
