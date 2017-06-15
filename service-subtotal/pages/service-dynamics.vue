4154a1fc92535a9d2d72062a1245c97a004f0010
hui.zhao <hui.zhao@mei1.com>
2017-06-15 11:52:44 +0800

    新增服务动态页面以及服务动态路由

---
+++ service-subtotal/pages/service-dynamics.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    5555
+        <!-- 服务动态页面 -->
+    </div>
+</template>
+<script>
+export default {
+    name: 'service-dynamics',
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {}
+};
+</script>
+<style lang="less">
+@import '~styles/_agile.less';
+</style>
