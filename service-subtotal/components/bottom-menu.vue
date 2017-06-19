1b26d5b55d7296a4eae7189bc46bef5055c55dee
hui.zhao <hui.zhao@mei1.com>
2017-06-19 19:10:54 +0800

    服务小计页面

---
+++ service-subtotal/components/bottom-menu.vue
@@ -0,0 +1,56 @@
+<template>
+    <div layout="row" layout-align="center center" class="fiexd-bottom">
+        <a @click="link" v-for="item in clickAble" flex="{{flex}}">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#{{item.name}}"></use>
+            </svg>
+            {{text}}
+        </a>
+    </div>
+</template>
+<script>
+export default {
+    name: '',
+    data() {
+        return {}
+    },
+    props: {
+        clickAble: {
+            type: Array,
+            default: [{
+                name: '返回',
+                icon: 'icon-left'
+            }, {
+                name: '门店',
+                icon: 'icon-fangzi-copy'
+            }, {
+                name: '时间',
+                icon: 'icon-shijian'
+            }, {
+                name: '状态',
+                icon: 'icon-caijitongji'
+            }]
+        },
+        flex: {
+            type: Number,
+            default: 25
+        }
+    },
+    methods: {
+        link(evt) {
+            this.$emit('click', evt);
+        }
+    }
+
+}
+</script>
+<style lang="less">
+@import '../styles/_agile';
+.fiexd-bottom {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    .icon {color: }
+}
+</style>
