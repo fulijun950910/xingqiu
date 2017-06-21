b7a6dfde9bb58d83f40311a2a262bb312979aa6d
jczzq <1195262383@qq.com>
2017-06-21 13:58:24 +0800

    组件优化

--- service-subtotal/components/m-pictures.vue
+++ service-subtotal/components/m-pictures.vue
@@ -1,10 +1,11 @@
 <template>
     <!-- 图片上传 -->
     <div class="m-pictures">
-        <div layout="row" layout-align="start center" flex-wrap="wrap">
-            <div :class="['c-picture-item', ('c-picture-item-'+column)]" v-for="(item,cid) in pictures" :key="cid" :ref="'picture_'+cid">
+        <transition-group name="slide-fade" tag="div" mode="out-in">
+            <div :class="['c-picture-item', ('c-picture-item-'+column)]" 
+                v-for="(item,cid) in pictures" :key="cid" :ref="'picture_'+cid">
                 <!-- 合同图片 -->
-                <m-picture :index="cid" @submit="submitPicture" ></m-picture>
+                <m-picture :index="cid" :picture="item" @submit="submitPicture"></m-picture>
                 
                 <!-- 移除 合同图片 -->
                 <p v-if="hasRemove" class="remove-btn" @click.stop="removePicture(cid)">
@@ -13,22 +14,20 @@
                     </svg>
                 </p>
             </div>
-            <div v-if="hasAdd && pictures.length<maxCount" @click="addPicture(null)"
-            :class="['c-picture-item', 'c-picture-item-add', ('c-picture-item-'+column)]" >
-                <div class="ava"  layout="column" layout-align="center center">
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-add"></use>
-                    </svg>
-                </div>
-            </div>
-        </div>
-        <p v-if="hasCounter" class="txt-tips">{{pictures.length}} / {{maxCount}}张</p>
+        </transition-group>
+        <p v-if="hasCounter" class="txt-tips">
+            <span class="add-pic" @click="addPicture(null)">
+                <svg class="icon" aria-hidden="true">  <use xlink:href="#icon-add"></use>  </svg>
+                传图片
+            </span>
+            <span class="pull-right">{{pictures.length}} / {{maxCount}}张</span>
+        </p>
     </div>
 </template>
 
 <script>
     /** 多个图片上传
-    * m-pictures : 照片列表
+    * pictures : 照片列表
     * has-add : 能否添加照片
     * has-remove : 能否移除照片
     * has-counter : 是否需要底部状态栏
@@ -36,6 +35,7 @@
     * min-count : 最少几张图片(0)
     * column： 每行排列几个 （1~3）
     *
+    * add : 添加某张图片（pic：要添加的图片, index：插入指定的下标）
     * remove ： 移除某张图片
     */
     import $ from 'jquery';
@@ -72,16 +72,7 @@
             },
             column: {
                 type: Number,
-                default: 3,
-                validator(value) {
-                    if (value > 3) {
-                        return 3;
-                    } else if (value < 1) {
-                        return 1;
-                    } else {
-                        return value;
-                    }
-                }
+                default: 3
             }
         },
         components: {
@@ -96,34 +87,67 @@
             submitPicture(img, idx) {
                 var self = this;
                 let key = 'picture_' + idx;
-                var newdiv2;
+                var newDiv;
                 if (this.$refs[key][0].lastElementChild.className == 'img-loading') {
-                    newdiv2 = self.$refs[key][0].lastElementChild;
+                    newDiv = self.$refs[key][0].lastElementChild;
                 } else {
-                    newdiv2 = document.createElement('div');
-                    newdiv2.className = 'img-loading';
+                    newDiv = document.createElement('div');
+                    newDiv.className = 'img-loading';
                 }
-                newdiv2.style.height = '100%';
-                newdiv2.innerText = '正在上传...';
-                self.$refs[key][0].appendChild(newdiv2);
+                newDiv.style.height = '100%';
+                newDiv.innerText = '正在上传...';
+                self.$refs[key][0].appendChild(newDiv);
                 api_file.uploadImage(img).then(res => {
                     // 上传成功
                     self.pictures[idx].base64 = null;
                     self.pictures[idx].id = res.data;
-                    newdiv2.innerText = '上传成功！';
-                    newdiv2.style.height = '24px';
+                    newDiv.innerText = '上传成功！';
+                    newDiv.style.height = '24px';
+                    window.setTimeout(() => {
+                        $(newDiv).animate({
+                            width: 0
+                        }, 300, function() {
+                            $(this).remove();
+                        });
+                    }, 3000);
                 }, res => {
                     // 上传失败
-                    newdiv2.innerText = '上传失败！';
                     self.pictures[idx].base64 = null;
+                    newDiv.innerText = '上传失败！';
+                    newDiv.style.height = '24px';
                     window.setTimeout(() => {
-                        newdiv2.style.height = '24px';
-                    }, 1500);
+                        $(newDiv).animate({
+                            width: 0
+                        }, 300, function() {
+                            $(this).remove();
+                        });
+                    }, 3000);
                 });
+            },
+            // 添加图片(最多9张)
+            addPicture(index) {
+                let imgNum = this.pictures.length;
+                if (imgNum < this.maxCount) {
+                    // 通知父组件添加图片对象
+                    this.$emit('add', {
+                        id: null,
+                        base64: null
+                    }, index);
+                    // 自动点击
+                    this.$nextTick(() => {
+                        let ava = this.$refs['picture_' + (this.pictures.length - 1)][0];
+                        let imgInput = $(ava).find('input[type=file]').get(0);
+                        var event = document.createEvent('Event');
+                        event.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                        imgInput.dispatchEvent(event);
+                    });
+                } else {
+                    this.$toast('最多上传' + this.maxCount + '张图片');
+                }
             },
             // 删除图片
             removePicture(imgId) {
-                // 先清除图片上的标签
+                // 先清除当前图片上的标签
                 let ava = this.$refs['picture_' + imgId];
                 $(ava).find('.img-loading').remove();
                 // 通知父组件移除图片对象
@@ -145,27 +169,6 @@
                 if (index !== this.pictures.length - 1) {
                     this.swapItems(this.pictures, index, index + 1);
                 }
-            },
-            // 添加图片(最多9张)
-            addPicture(index) {
-                let imgNum = this.pictures.length;
-                if (imgNum < 9) {
-                    if (index === null) {
-                        this.pictures.push({
-                            id: null,
-                            base64: null
-                        });
-                    } else {
-                        this.pictures.splice(index + 1, 0, {
-                            id: null,
-                            base64: null
-                        });
-                    }
-                    let ava = this.$refs['picture_' + this.pictures.length];
-                    $(ava).find('input[type=file]').trigger('click');
-                } else {
-                    this.$toast('最多上传' + this.maxCount + '张图片');
-                }
             }
         }
     };
@@ -180,7 +183,7 @@
             border: 1px solid @card-gray;
             box-shadow:0 0 5px 1px @color-bg;
             color:fade(@gray,50%);
-            
+            clear: both;
             .remove-btn{
                 width:24px;
                 height:24px;
@@ -199,6 +202,8 @@
             .img-loading{
                 transition: height .3s ease-in;
                 position: absolute;
+                white-space: nowrap;
+                overflow: hidden;
                 top: 0;
                 left: 0;
                 background-color: rgba(0, 0, 0, 0.51);
@@ -215,10 +220,10 @@
                 font-size: 12px;
                 z-index:6;
             }
-            &.c-picture-item-add{
-                background-color: #f8f8f8;
-                border: 1px dashed #ccc;
-            }
+        }
+        .c-picture-item-add{
+            background-color: #f8f8f8;
+            border: 1px dashed #ccc;
         }
         .c-picture-item-1 {
             width:92vw;
@@ -234,6 +239,14 @@
             width:28vw;
             height:28vw;
             margin: 4vw 0 0 4vw;
+        }
+        .txt-tips{
+            color: @extra-light-gray;
+            text-align: left;
+            padding: 10px @l32;
+            .add-pic{
+                color: @color-main;
+            }
         }
     }
 </style>
