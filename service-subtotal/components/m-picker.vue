92f83e6f875a7b2632f6a6001bb34a6b5e742696
尹现伟 <xianwei.yin@mei1.com>
2017-06-22 14:56:25 +0800

    m-picker

---
+++ service-subtotal/components/m-picker.vue
@@ -0,0 +1,68 @@
+<template>
+    <mt-popup v-model="currentValue" position="bottom" class="picker-panel">
+        <mt-picker :show-toolbar="true" :slots="slots" @change="onValuesChange">
+            <div layout="row" layout-align="space-between center" class="toolbar">
+                <a class="steel-gray picker-btn" @click="currentValue = false;">取消</a>
+                <a class="picker-btn" @click="confirm">确定</a>
+            </div>
+        </mt-picker>
+    </mt-popup>
+</template>
+<script>
+import Vue from 'vue';
+import {
+    Popup,
+    Picker
+} from 'mint-ui';
+Vue.component(Popup.name, Popup);
+Vue.component(Picker.name, Picker);
+
+export default {
+    name: 'm-picker',
+    data() {
+        return {
+            changeItem: null
+        };
+    },
+    props: {
+        slots: {
+            type: Array,
+            default: []
+        },
+        value: Boolean,
+        selectedItem: null
+    },
+    methods: {
+        onValuesChange(picker, values) {
+            this.changeItem = values;
+        },
+        confirm() {
+            this.$emit('update:selectedItem', this.$knife.deepCopy(this.changeItem, []));
+            this.currentValue = false;
+        }
+    },
+    computed: {
+        currentValue: {
+            get() {
+                return this.value;
+            },
+            set(val) {
+                this.$emit('input', val);
+            }
+        }
+    }
+};
+</script>
+<style lang="less">
+@import '~styles/_agile';
+.picker-panel {
+    width: 100%;
+    .toolbar {
+        border-bottom: 1px solid #eee;
+        /*no*/
+        .picker-btn {
+            padding: @l16 @l32;
+        }
+    }
+}
+</style>
