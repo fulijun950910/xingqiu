86d5a90327f99bc22638d81ad417303f9f7a0703
hui.zhao <hui.zhao@mei1.com>
2017-06-23 15:00:59 +0800

    添加fromnow过滤器

--- service-subtotal/filter/moment-filter.js
+++ service-subtotal/filter/moment-filter.js
@@ -4,3 +4,6 @@ import moment from 'moment';
 Vue.filter('amDateFormat', (value, format = 'YYYY-MM-DD HH:mm:ss') => {
     return moment(value).format(format);
 });
+Vue.filter('fromnow', (value) => {
+    return moment(value).startOf('hour').fromNow();
+});
