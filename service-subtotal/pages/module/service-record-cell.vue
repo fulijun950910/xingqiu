6e9272f2b8a21c638aba6d9f6167f162557e40de
尹现伟 <xianwei.yin@mei1.com>
2017-06-23 17:42:56 +0800

    服务列表页面

---
+++ service-subtotal/pages/module/service-record-cell.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="service-record-cell" layout="row" layout-align="center center">
+        <img :src="require('assets/imgs/avatar.png')" class="avatar">
+        <div flex class="content">
+            <span class="fs24 dark-gray">No.321 张三丰</span>
+            <p class="extra-light-black">美白基础护理</p>
+        </div>
+        <div>
+            <p class="fs24 gray">今天</p>
+            <p class="dark-gray">20:00</p>
+        </div>
+        <span class="h-line"></span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'service-record-cell',
+    props: ['value'],
+    data() {
+        return {};
+    },
+    mounted() {},
+    methods: {}
+};
+</script>
+<style lang="less">
+@import '~styles/_agile';
+.service-record-cell {
+    padding: @l32;
+    position: relative;
+    .avatar {
+        width: 50 / 2px;
+        height: 50 / 2px;
+        border-radius: 50 / 2px;
+    }
+    .content {
+        padding-left: @l32;
+        padding-right: @l16;
+    }
+    .h-line {
+        right: 0;
+        bottom: 0;
+        left: 24 + @l32*2;
+        position: absolute;
+    }
+}
+</style>
