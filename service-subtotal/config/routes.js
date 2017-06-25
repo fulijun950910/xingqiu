351c310e0c28810fc16e25f70ae18d2d7314a1a9
jczzq <1195262383@qq.com>
2017-06-25 22:19:40 +0800

    代码注释调整规范

--- service-subtotal/config/routes.js
+++ service-subtotal/config/routes.js
@@ -18,51 +18,51 @@ export default [{
     component: dataView,
     meta: { auth: true, level: 2 }
 }, {
-    path: '/return-visit-edit/:recordId',   // 客户关怀 编辑
+    path: '/return-visit-edit/:recordId', // 客户关怀 编辑
     name: 'return-visit-edit',
     component: returnVisitEdit,
     meta: { auth: true, level: 4 }
 }, {
-    path: '/member-list',   // 会员列表
+    path: '/member-list', // 会员列表
     name: 'member-list',
     component: memberList,
     meta: { auth: true, level: 3 }
 }, {
-    path: '/return-visit-ranking-list',     // 查看客户关怀（查看服务小计页面缺失）
+    path: '/return-visit-ranking-list', // 查看客户关怀（查看服务小计页面缺失）
     name: 'return-visit-ranking-list',
     component: returnVisitRankingList,
     meta: { auth: true, level: 3 }
 }, {
-    path: '/member-maintain',   // 客户维护
+    path: '/member-maintain', // 客户维护
     name: 'member-maintain',
     component: memberMaintain,
     meta: { auth: true, level: 2 }
 }, {
-    path: '/service-record-edit/:recordId',     // 服务小计 编辑
+    path: '/service-record-edit/:recordId', // 服务小计 编辑
     name: 'service-record-edit',
     component: serviceRecordEdit,
     meta: { auth: true, level: 4 }
 }, {
-    path: '/service-record-list',       // 服务小计 列表
+    path: '/service-record-list', // 服务小计 列表
     name: 'service-record-list',
     component: serviceRecordList,
     meta: { auth: true, level: 3 }
 }, {
-    path: '/record-finish',     // 记录完成
+    path: '/record-finish', // 记录完成
     name: 'record-finish',
     component: recordFinish,
     meta: { auth: true, level: 5 }
 }, {
-    path: '/unrecorded-service-list',   // 未记录服务
+    path: '/unrecorded-service-list', // 未记录服务
     name: 'unrecorded-service-list',
     component: unrecordedServiceList,
     meta: { auth: true, level: 3 }
 }, {
-    path: '/service-dynamics',      // 服务动态（首页）
+    path: '/service-dynamics', // 服务动态（首页）
     name: 'service-dynamics',
     component: serviceDynamics,
     meta: { auth: true, level: 1 }
 }, {
-    path: '*',      // 服务动态（默认）
+    path: '*', // 服务动态（默认）
     redirect: '/service-dynamics'
 }];
