8bede65d6b81e69b87647a2885f1147c84a5fe8f
hui.zhao <hui.zhao@mei1.com>
2017-06-26 11:54:54 +0800

    添加员工查询列表接口

--- service-subtotal/services/api.serviceNote.js
+++ service-subtotal/services/api.serviceNote.js
@@ -1,5 +1,4 @@
 import request from './request.js';
-import Q from 'q';
 
 export default {
     /**
@@ -15,6 +14,14 @@ export default {
      * 说明：服务小计修改接口；修改会员标签，标签热度变更；包含日期提醒功能，即定时提醒推送功能；（需考虑修改提醒时是否变更提醒条件，如果已执行过的提醒任务的状态处理）
      */
     updateServiceNote: function(data) {
+        let url = '/api/wechatbusinessassists/serviceNoteUpdate';
+        return request(url, data, 'post');
+    },
+    /**
+     * 员工查询列表
+     * 说明：通用接口，包含员工搜索查询
+     */
+    employeeList: function(data) {
         let url = '/api/wechatbusinessassists/serviceNoteUpdate';
         return request(url, data, 'post');
     }
