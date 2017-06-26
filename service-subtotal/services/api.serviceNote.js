2ce62f4e660136626307d12a91807e6ad927d38f
jczzq <1195262383@qq.com>
2017-06-26 10:29:15 +0800

    添加服务小计service类

---
+++ service-subtotal/services/api.serviceNote.js
@@ -0,0 +1,21 @@
+import request from './request.js';
+import Q from 'q';
+
+export default {
+    /**
+     * 服务小计添加
+     * 说明：服务小计添加接口；包含会员标签管理，打标签功能；包含日期提醒功能，即定时提醒推送功能；
+     */
+    createServiceNote: function(data) {
+        let url = '/api/wechatbusinessassists/serviceNote';
+        return request(url, data, 'post');
+    },
+    /**
+     * 修改小计添加
+     * 说明：服务小计修改接口；修改会员标签，标签热度变更；包含日期提醒功能，即定时提醒推送功能；（需考虑修改提醒时是否变更提醒条件，如果已执行过的提醒任务的状态处理）
+     */
+    updateServiceNote: function(data) {
+        let url = '/api/wechatbusinessassists/serviceNoteUpdate';
+        return request(url, data, 'post');
+    }
+};
