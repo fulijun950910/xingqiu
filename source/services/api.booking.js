c0e8504644bcc97addfd0287afbc353058172940
pan.xu <pan.xu@mei1.com>
2018-03-07 10:41:41 +0800

    删除多余接口

--- source/services/api.booking.js
+++ source/services/api.booking.js
@@ -1,24 +1,8 @@
 import request from './request.js';
 
 export default {
-    searchItem(data) {
-        var url = '/api/serviceItem/search';
-        if (data && data.query) {
-            for (var i = 0; i < data.query.length; i++) {
-                if (!data.query[i].value) {
-                    data.query.splice(i, 1);
-                    i--;
-                }
-            };
-            return request(url, data, 'post');
-        }
-    },
     getStoreInfo(id) {
         var url = `/api/store/${id}`;
-        return request(url, null, 'get');
-    },
-    getBookingDetail(id) {
-        var url = '/api/member/detail/1304825151812951';
         return request(url, null, 'get');
     },
     getAppointment(id) { // 获取预约详细信息
