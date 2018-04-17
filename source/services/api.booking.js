f528d2ec411147033183595319ad005401b0db28
pan.xu <pan.xu@mei1.com>
2018-04-17 14:51:36 +0800

    添加搜索项目接口

--- source/services/api.booking.js
+++ source/services/api.booking.js
@@ -1,6 +1,18 @@
 import request from './request.js';
 
 export default {
+    searchItem(data) {
+        var url = '/api/serviceItem/search';
+        if (data && data.query) {
+            for (var i = 0; i < data.query.length; i++) {
+                if (!data.query[i].value) {
+                    data.query.splice(i, 1);
+                    i--;
+                }
+            };
+            return request(url, data, 'post');
+        }
+    },
     getStoreInfo(id) {
         var url = `/api/store/${id}`;
         return request(url, null, 'get');
