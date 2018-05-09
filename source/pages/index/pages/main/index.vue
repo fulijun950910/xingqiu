3213d0c7d534b350cdc2f6dd30a01a571ba8535f
pan.xu <pan.xu@mei1.com>
2018-05-09 17:43:46 +0800

    美问星球

---
+++ source/pages/index/pages/main/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'main',
+        data() {
+            return {
+            };
+        },
+        mounted() {
+        },
+        methods: {}
+    };
+</script>
+
+<style scoped lang='less'>
+    @import '~styles/_style';
+
+</style>
