0dd34ff78bfe8d916a794e53997017042b006e24
pan.xu <pan.xu@mei1.com>
2018-05-17 11:11:36 +0800

    美问星球

--- source/pages/index/pages/getRule/index.vue
+++ source/pages/index/pages/getRule/index.vue
@@ -15,140 +15,178 @@
                 <m-icon class="" xlink="#icon-right-bold"></m-icon>
             </div>
         </div>
-        <div class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox2" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-qianyue"></m-icon>
+        <div v-for="item in dataList">
+            <div v-if="item.missionCode == 'FIRST_LOGIN'" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox2" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-qianyue"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>{{item.missionName}}</div>
+                        <div class="fs24 dark-gray">{{item.missionDesc}}</div>
+                    </div>
                 </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>首次登陆</div>
-                    <div class="fs24 dark-gray"><span class="color-red-lighten">+10</span>美豆豆</div>
+                <div v-if="item.completed" class="light-gray">
+                    已获得
                 </div>
             </div>
-            <div class="light-gray">
-                已获得
+            <div v-else-if='false' class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox3" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-hezuo"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>{{item.missionName}}</div>
+                        <div class="fs24 dark-gray">{{item.missionDesc}}</div>
+                    </div>
+                </div>
+                <div  class="light-gray">
+                    已获得
+                </div>
+            </div>
+            <div v-else-if="item.missionCode == 'SIGN'" @click="goCheckIn" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox4" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-qiandao"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>{{item.missionName}}</div>
+                        <div class="fs24 dark-gray">{{item.missionDesc}}</div>
+                    </div>
+                </div>
+                <div v-if="item.completed" class="light-gray">
+                    已获得
+                </div>
+                <div v-else class="fs24 color-green">
+                    <span>去签到</span>
+                    <m-icon class="" xlink="#icon-right-bold"></m-icon>
+                </div>
+            </div>
+
+            <div v-else-if="item.missionCode == 'RELEASE_PROMOTION'" @click="goPromotion" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox5" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-zengsong"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>{{item.missionName}}（{{item.value}}/{{Math.floor(item.conditionValue) || 0}}）</div>
+                        <div class="fs24 dark-gray">{{item.missionDesc}}</div>
+                    </div>
+                </div>
+                <div v-if="item.completed" class="light-gray">
+                    已获得
+                </div>
+                <div v-else class="fs24 color-green">
+                    <span>去发布</span>
+                    <m-icon class="" xlink="#icon-right-bold"></m-icon>
+                </div>
+            </div>
+            <div v-else-if="item.missionCode == 'ORDER_COUNT'" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox6" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-huangguan"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>{{item.missionName}}</div>
+                        <div class="fs24 dark-gray">{{item.missionDesc}}</div>
+                    </div>
+                </div>
+                <div v-if="item.completed" class="light-gray">
+                    已获得
+                </div>
+                <div class="fs24 color-green">
+                </div>
+            </div>
+            <div v-else-if="item.missionCode == 'POS_TRADE'" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox6" layout="row" layout-align="center center">
+                        <img :src="require('assets/imgs/index/20180510170401.jpg')" alt="">
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>{{item.missionName}}</div>
+                        <div class="fs24 dark-gray">{{item.missionDesc}}</div>
+                    </div>
+                </div>
+                <div v-if="item.completed" class="light-gray">
+                    已获得
+                </div>
+                <div class="fs24 color-green">
+                </div>
+            </div>
+            <div v-else-if="false" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox7" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-ai246"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>介绍商户入驻</div>
+                        <div class="fs24 dark-gray">介绍1个商户<span class="color-red-lighten">+1000</span>美豆豆</div>
+                    </div>
+                </div>
+                <div class="fs24 color-green">
+                </div>
+            </div>
+            <div v-if="false" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
+                <div layout="row" layout-align="start start">
+                    <div class="iconBox iconBox8" layout="row" layout-align="center center">
+                        <m-icon class="" xlink="#icon-weibiaoti2fuzhi02"></m-icon>
+                    </div>
+                    <span>&nbsp;&nbsp;</span>
+                    <div>
+                        <div>作为异业宿主发券成功</div>
+                        <div class="fs24 dark-gray">发券1次<span class="color-red-lighten">+10</span>美豆豆</div>
+                    </div>
+                </div>
+                <div class="fs24 color-green">
+                    <span>去发券</span>
+                    <m-icon class="" xlink="#icon-right-bold"></m-icon>
+                </div>
             </div>
         </div>
 
-        <div v-if='false' class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox3" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-hezuo"></m-icon>
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>异业共赢</div>
-                    <div class="fs24 dark-gray"><span class="color-red-lighten">+50</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="light-gray">
-                已获得
-            </div>
-        </div>
-        <div @click="goCheckIn" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox4" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-qiandao"></m-icon>
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>每日签到</div>
-                    <div class="fs24 dark-gray">每日1豆，连续签到7天<span class="color-red-lighten">+10</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="fs24 color-green">
-                <span>去签到</span>
-                <m-icon class="" xlink="#icon-right-bold"></m-icon>
-            </div>
-        </div>
 
-        <div @click="goPromotion" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox5" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-zengsong"></m-icon>
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>发布微信营销活动（0/2）</div>
-                    <div class="fs24 dark-gray">每月完成2个营销活动<span class="color-red-lighten">+5</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="fs24 color-green">
-                <span>去发布</span>
-                <m-icon class="" xlink="#icon-right-bold"></m-icon>
-            </div>
-        </div>
-        <div class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox6" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-huangguan"></m-icon>
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>明星商户</div>
-                    <div class="fs24 dark-gray">每月完成100个订单<span class="color-red-lighten">+5</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="fs24 color-green">
-            </div>
-        </div>
-        <div class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox6" layout="row" layout-align="center center">
-                    <img :src="require('assets/imgs/index/20180510170401.jpg')" alt="">
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>POS交易</div>
-                    <div class="fs24 dark-gray">每交易1千元<span class="color-red-lighten">+1</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="fs24 color-green">
-            </div>
-        </div>
-        <div class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox7" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-ai246"></m-icon>
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>介绍商户入驻</div>
-                    <div class="fs24 dark-gray">介绍1个商户<span class="color-red-lighten">+1000</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="fs24 color-green">
-            </div>
-        </div>
-        <div v-if="false" class="list-item error cell cell-box" layout="row" layout-align="space-between center">
-            <div layout="row" layout-align="start start">
-                <div class="iconBox iconBox8" layout="row" layout-align="center center">
-                    <m-icon class="" xlink="#icon-weibiaoti2fuzhi02"></m-icon>
-                </div>
-                <span>&nbsp;&nbsp;</span>
-                <div>
-                    <div>作为异业宿主发券成功</div>
-                    <div class="fs24 dark-gray">发券1次<span class="color-red-lighten">+10</span>美豆豆</div>
-                </div>
-            </div>
-            <div class="fs24 color-green">
-                <span>去发券</span>
-                <m-icon class="" xlink="#icon-right-bold"></m-icon>
-            </div>
-        </div>
+
     </div>
 </template>
 
 <script>
+    import api_party from 'services/api.party';
+
     export default {
         name: 'getRule',
         data() {
-            return {};
+            return {
+                dataList: [],
+                dataModel: {
+                    FIRST_LOGIN: {},
+                    SIGN: {},
+                    RELEASE_PROMOTION: {},
+                    ORDER_COUNT: {},
+                    POS_TRADE: {}
+                }
+            };
         },
         mounted() {
+            this.loadData();
         },
         methods: {
+            loadData() {
+                api_party.getMissionList(this.$store.state.party.partyId).then(res => {
+                    this.dataList = res.data;
+                    res.data.forEach(item => {
+                        if (item.missionCode) {
+                            this.dataModel[item.missionCode] = item;
+                        }
+                    });
+                });
+            },
             goCheckIn() {
                 this.$router.push({name: 'checkIn'});
             },
