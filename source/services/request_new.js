e4692e242d8e21b11da9189a0cf9ad2a445dc983
pan.xu <pan.xu@mei1.com>
2018-05-17 11:39:46 +0800

    美问星球

--- source/services/request_new.js
+++ source/services/request_new.js
@@ -14,8 +14,8 @@ export default function(url, method, data, messageFlag = true) {
         } else {
             Vue.prototype.$indicator.close();
             if (messageFlag) {
-                if (res.message) {
-                    Vue.prototype.$toast(res.message);
+                if (res.data.message) {
+                    Vue.prototype.$toast(res.data.message);
                 } else {
                     Vue.prototype.$toast('服务器开小差，请稍后再试');
                 }
