593d1823f75ddc565a5d423f47bbea8e2a54389c
fred_zhao <hui.zhao@mei1.com>
2018-05-18 12:49:45 +0800

    fix[美博会]

--- source/pages/shop/pages/index.vue
+++ source/pages/shop/pages/index.vue
@@ -181,7 +181,7 @@ export default {
 <style lang="less">
 @import '~styles/_agile';
 .main-container.index{
-    padding: 100px 10px 50px 10px;
+    padding: 120px 10px 50px 10px;
     background: white;
     position: relative;
     width: 100vw;
@@ -215,7 +215,7 @@ export default {
          position: absolute;
          left: 10px;
          right:10px;
-         top:60px;
+         top:80px;
          background: white;
          z-index: 999;;
         .tab-item{
