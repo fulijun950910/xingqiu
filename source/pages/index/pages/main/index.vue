6dc2bfb655bb8577a5fa7603ef9549d5154ed5c0
pan.xu <pan.xu@mei1.com>
2018-06-04 19:58:02 +0800

    调试

--- source/pages/index/pages/main/index.vue
+++ source/pages/index/pages/main/index.vue
@@ -120,8 +120,8 @@
             },
             async checkUser() {
                 var deferred = Q.defer();
-                console.log(JSOn.stringify(this.$store.state));
-                console.log(JSOn.stringify(localStorage.employee));
+                console.log(JSON.stringify(this.$store.state));
+                console.log(JSON.stringify(localStorage.employee));
                 if (!this.$store.state || !this.$store.state.user || !this.$store.state.party || !this.$store.state.party.partyId) {
                     let res = await api_signIn.getEmployeeInfo();
                     if (res.success && res.data) {
