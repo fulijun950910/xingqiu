d8d718fb0a45c6419870bcde8b8909fd065e80c3
fred_zhao <hui.zhao@mei1.com>
2018-06-13 17:40:02 +0800

    订单优化

--- source/pages/integral-mall/pages/list/order-list.vue
+++ source/pages/integral-mall/pages/list/order-list.vue
@@ -54,7 +54,7 @@
                 <div flex v-if="item.tradeDelivery">
                     <div class="fs28 color-gray">配送地址：{{item.tradeDelivery.fullAddress}}</div>
                     <div class="fs28 color-gray">快递公司：{{item.tradeDelivery.logisticsCompany}}</div>
-                     <div class="fs28 color-gray">快递公司：{{item.tradeDelivery.logisticsCompany}}</div>
+                     <div class="fs28 color-gray">快递单号：{{item.tradeDelivery.logisticsNo}}</div>
                 </div>
                 <div layout="row" layout-align="space-between center" class="bottom">
                     <div class="fs24 color-gray">{{item.createdTime | amDateFormat}}</div>
@@ -131,7 +131,7 @@ export default {
                             value: 1
                         },
                         {
-                            name: '短信签名',
+                            name: '虚拟商品',
                             value: 2
                         },
                         {
