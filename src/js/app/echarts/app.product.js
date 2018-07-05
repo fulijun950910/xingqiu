68c7b216827676b27cb55b217b757d33484d8ce1
fred_zhao <hui.zhao@mei1.com>
2018-07-05 11:50:38 +0800

    去掉前端排序

--- src/js/app/echarts/app.product.js
+++ src/js/app/echarts/app.product.js
@@ -20,7 +20,7 @@ app.productEcharts = {
     },
     initCystomDate: function(type) {
         app.tools.initCystomDate(type, app.productEcharts.productEchartsIdName);
-        //确定自定义时间选择
+        // 确定自定义时间选择
         $('.cystomDate').on('click', '.saveDate', function() {
             $('.product_echarts  .mask').click();
             app.productEcharts.reportShow(4, 'date');
@@ -28,7 +28,7 @@ app.productEcharts = {
     },
     initStoreList: function() {
         app.tools.initStoreList(app.productEcharts.productEchartsIdName);
-        //点击切换门店
+        // 点击切换门店
         $('.product_echarts .storeLists .stores').on('click', 'span', function(event) {
             $('.product_echarts .storeLists span').removeClass('active').find('i').remove();
             $(this).addClass('active').append('<i></i>');
@@ -37,7 +37,7 @@ app.productEcharts = {
         });
     },
     userdata: function() {
-        //初始化用户信息
+        // 初始化用户信息
         return new Promise(function(resolve, reject) {
             app.userinfo.getEmployee().then(function(user) {
                 resolve(user);
@@ -46,12 +46,12 @@ app.productEcharts = {
     },
     init: function() {
         app.productEcharts.userdata().then(function(userDate) {
-            app.productEcharts.reportShow("", "init");
+            app.productEcharts.reportShow('', 'init');
         }, function(error) {
             app.alert(error);
         });
     },
-    //初始数据
+    // 初始数据
     reportShow: function(data, type) {
         var employee = null;
         if (JSON.parse(localStorage.employee)) {
@@ -68,7 +68,7 @@ app.productEcharts = {
         };
         query.queryData = {
             itemSalesDetail: true
-        }
+        };
         var storeIds = $('.storeList .store_name').attr('data-storeId');
         if (storeIds && storeIds.trim()) {
             query.storeIds = $('.storeList .store_name').attr('data-storeId');
@@ -78,10 +78,10 @@ app.productEcharts = {
         var dataType = $('.dateList .date_name').attr('data-type');
         if (dataType && dataType.trim()) {
             query.dataType = $('.dateList .date_name').attr('data-type');
-            app.tools.getDateType(query.dataType, data, app.productEcharts.echartsDate)
+            app.tools.getDateType(query.dataType, data, app.productEcharts.echartsDate);
         } else {
             query.dataType = 1;
-            query.startDate = moment().format('YYYY-MM-DD ') + "00:00:00";
+            query.startDate = moment().format('YYYY-MM-DD ') + '00:00:00';
             query.endDate = moment().format('YYYY-MM-DD HH:mm:ss');
         }
         switch (type) {
@@ -89,10 +89,10 @@ app.productEcharts = {
                 query.storeIds = data;
                 break;
             case 'date':
-                app.tools.getDateType(data, query, app.productEcharts.echartsDate)
+                app.tools.getDateType(data, query, app.productEcharts.echartsDate);
                 query.dataType = data;
                 break;
-                //初始第一次页面
+                // 初始第一次页面
             case 'init':
                 var employeeInfo = null;
                 if (localStorage.performanceInfo && JSON.parse(localStorage.performanceInfo)) {
@@ -100,7 +100,7 @@ app.productEcharts = {
                     query.dataType = employeeInfo.dataType;
                 }
                 query.storeIds = employeeInfo.performanceStoreIds;
-                //自定义
+                // 自定义
                 if (employeeInfo.dataType == 4) {
                     query.startDate = employeeInfo.startDate;
                     query.endDate = employeeInfo.endDate;
@@ -109,15 +109,15 @@ app.productEcharts = {
                         endDate: query.endDate
                     };
                 } else {
-                    app.tools.getDateType(employeeInfo.dataType, query, app.productEcharts.echartsDate)
+                    app.tools.getDateType(employeeInfo.dataType, query, app.productEcharts.echartsDate);
                 }
                 break;
         }
         results.storeList = employee.storeList;
         results.storeIds = employee.storeIds;
         results.storeId = query.storeIds;
-        //普通员工
-        if (employee.role == "wechat_business_normal") {
+        // 普通员工
+        if (employee.role == 'wechat_business_normal') {
             var tmplhtml = $('#tmpl-product-model').html();
             var resultTmpl = tmpl(tmplhtml, results);
             $('#tmpl-product').html(resultTmpl);
@@ -129,16 +129,16 @@ app.productEcharts = {
             return;
         }
         app.productEcharts.show(query).then(function(result) {
-            for (var i = 0; i < result.data.itemSalesDetail.length; i++) {
-                for (var j = i + 1; j < result.data.itemSalesDetail.length; j++) {
-                    if (result.data.itemSalesDetail[i][2] + result.data.itemSalesDetail[i][3] > result.data.itemSalesDetail[j][2] + result.data.itemSalesDetail[j][3]) {
-                        var itemA = result.data.itemSalesDetail.splice(i, 1)[0]
-                        result.data.itemSalesDetail.splice(j, 0, itemA)
-                        var itemB = result.data.itemSalesDetail.splice(j - 1, 1)[0]
-                        result.data.itemSalesDetail.splice(i, 0, itemB)
-                    }
-                }
-            }
+            // for (var i = 0; i < result.data.itemSalesDetail.length; i++) {
+            //     for (var j = i + 1; j < result.data.itemSalesDetail.length; j++) {
+            //         if (result.data.itemSalesDetail[i][2] + result.data.itemSalesDetail[i][3] > result.data.itemSalesDetail[j][2] + result.data.itemSalesDetail[j][3]) {
+            //             var itemA = result.data.itemSalesDetail.splice(i, 1)[0];
+            //             result.data.itemSalesDetail.splice(j, 0, itemA);
+            //             var itemB = result.data.itemSalesDetail.splice(j - 1, 1)[0];
+            //             result.data.itemSalesDetail.splice(i, 0, itemB);
+            //         }
+            //     }
+            // }
             for (var i = result.data.itemSalesDetail.length - 1; i >= 0; i--) {
                 switch (result.data.itemSalesDetail[i][0]) {
                     case 'SERVICE_ITEM':
@@ -155,18 +155,18 @@ app.productEcharts = {
                         break;
                 }
             }
-            var tooBarIndex = $('.tool-tab ul li.active').index(); //还原选中tab
+            var tooBarIndex = $('.tool-tab ul li.active').index(); // 还原选中tab
             app.tools.changeTitle('品项数据分析');
             var tmplhtml = $('#tmpl-product-model').html();
             var resultTmpl = tmpl(tmplhtml, results);
             $('#tmpl-product').html(resultTmpl);
             app.tools.initSwiper(25);
-            app.productEcharts.initDate(query.dataType); //初始化时间
-            app.productEcharts.initStoreList(); //初始化门店
-            app.productEcharts.initCystomDate(query.dataType); //初始化自定义时间
+            app.productEcharts.initDate(query.dataType); // 初始化时间
+            app.productEcharts.initStoreList(); // 初始化门店
+            app.productEcharts.initCystomDate(query.dataType); // 初始化自定义时间
             $('.dateLists span').eq(parseInt(query.dataType) - 1).addClass('active');
             if (query.storeIds == results.storeIds) {
-                $('.storeLists span:first').addClass('active').append('<i></i>'); //日期名称
+                $('.storeLists span:first').addClass('active').append('<i></i>'); // 日期名称
             } else {
                 for (var i = 0; i <= results.storeList.length - 1; i++) {
                     var storeId = results.storeList[i].id;
@@ -176,18 +176,18 @@ app.productEcharts = {
                     }
                 }
             }
-            $('.tool-tab ul li').eq(tooBarIndex).click(); //还原选中tab
+            $('.tool-tab ul li').eq(tooBarIndex).click(); // 还原选中tab
             // 自定义名词
             app.api.personalNoun({
                 data: employee.merchantId,
                 success: function(msg) {
                     var psersonalNoun = app.api.baseNoun(msg.data);
-                    $('.personalProject').html(psersonalNoun.project); 
+                    $('.personalProject').html(psersonalNoun.project);
                 },
                 error: function(error) {
 
                 }
-            })
+            });
         }, function(error) {
             app.alert(error);
         });
@@ -213,4 +213,4 @@ app.productEcharts = {
             });
         });
     }
-}
+};
