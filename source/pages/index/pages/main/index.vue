9fb7c057dfc335ad5751cc3dcc3c23d3988a6018
尹现伟 <xianwei.yin@mei1.com>
2018-07-09 15:27:09 +0800

    fix: 修复登录判断错误的问题

--- source/pages/index/pages/main/index.vue
+++ source/pages/index/pages/main/index.vue
@@ -197,7 +197,7 @@
                     return;
                 }
                 await this.checkUser();
-                if (this.$store.state.user.merchant && (this.$store.state.user.merchant.functionVersion == 4 || this.$store.state.user.merchant.functionVersion == 5)) {
+                if (this.$store.state.merchant && (this.$store.state.merchant.functionVersion == 4 || this.$store.state.merchant.functionVersion == 5)) {
                     window.location.href = '/lite/index.html';
                 } else {
                     window.location.href = '/main.html#/index';
