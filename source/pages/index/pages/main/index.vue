344c4617cee445c481455144387983c360a4197e
fred_zhao <hui.zhao@mei1.com>
2018-07-24 16:35:03 +0800

    隐藏美博会商城

--- source/pages/index/pages/main/index.vue
+++ source/pages/index/pages/main/index.vue
@@ -206,17 +206,19 @@
                 this.$router.push({name: 'alliance'});
             },
             goMbh() {
-                if (this.$enviroment != 'development') {
-                    if (this.$store.state && this.$store.state.party && this.$store.state.party.partyId) {
-                        let openId = this.$knife.keyGetValue(window.location.search, 'openid');
-                        api_party.bandWeichat(this.$store.state.party.id, openId).then(msg=> {
-                            window.location.href = 'http://b2b.mei1.info/app/index.php?i=1&c=entry&eid=41&saasUID=1111';
+                // if (this.$enviroment === 'development') {
+                //     if (this.$store.state && this.$store.state.party && this.$store.state.party.partyId) {
+                //         let openId = this.$knife.keyGetValue(window.location.search, 'openid');
+                //         api_party.bandWeichat(this.$store.state.party.id, openId).then(msg=> {
+                //             window.location.href = `http://b2b.mei1.info/app/index.php?i=1&c=entry&eid=41&saasUID=${this.$store.state.party.id}`;
 
-                        }, msg=> {
+                //         }, msg=> {
 
-                        });
-                    }
-                }
+                //         });
+                //     }
+                // } else {
+                window.location.href = this.$rootPath + 'shop.html#/leader';
+                // }
             },
             async goWxbus() {
                 if (this.checkParty()) {
