6056d8d1c8fca9a25119b5c1d338d9211fa23318
尹现伟 <xianwei.yin@mei1.com>
2018-07-31 10:36:46 +0800

    add: 客户资源列表页面

---
+++ source/pages/service-subtotal/pages/customers/list.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>customers</div>
+</template>
+<script>
+export default {
+    name: 'customers',
+    props: {},
+    components: {},
+    computed: {},
+    data() {
+        return {};
+    },
+    mounted() {},
+    methods: {}
+};
+</script>
+<style lang="less">
+</style>
