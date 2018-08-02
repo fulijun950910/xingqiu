ea44ba5403b5b85741d4312df2665d45cb791110
fred_zhao <hui.zhao@mei1.com>
2018-08-02 15:11:15 +0800

    fix[#11266]

--- source/pages/integral-mall/pages/applicationMarket-form.vue
+++ source/pages/integral-mall/pages/applicationMarket-form.vue
@@ -1,78 +1,169 @@
 <template>
-    <div class="applicationMarket-form">
-        <div class="activity-detail">
-<div class="img-con">
-   <mt-swipe :auto="4000">
-  <mt-swipe-item v-for="(item, index) in chooseServiceItem.images" :key="index">
-   <img class="img-auto" :src="item | nSrc(require('assets/imgs/female.png'))" alt="">      
-  </mt-swipe-item>
-</mt-swipe>
-</div>
-<div class="description">
-    <div class="des-title fwb fs34 color-black">{{chooseServiceItem.name}}</div>
-    <div class="des-detail fs24 color-gray" v-html="chooseServiceItem.detail"></div>
-</div>
-</div>
-<!-- <div class="form-title text-center">
-    <div class="fs40 color-black">培训申请表</div>
-    <p class="fs24 color-gray">亲爱的朋友，只有填完表但我们才能更好的为您服务哦</p>
-</div> -->
-<div class="form-con">
-    <div flex class="form-item">
-        <div class="label fs24 color-black">商户名称</div>
-        <div flex>
-            <input flex class="color-black fs30" type="text" v-model="parameter.merchantName">
+<div class="applicationMarket-form">
+    <div class="activity-detail">
+        <div class="img-con">
+            <mt-swipe :auto="4000">
+                <mt-swipe-item v-for="(item, index) in chooseServiceItem.images" :key="index">
+                    <img class="img-auto" :src="item | nSrc(require('assets/imgs/female.png'))" alt="">
+                </mt-swipe-item>
+            </mt-swipe>
+        </div>
+        <div class="description">
+            <div class="des-title fwb fs34 color-black">{{chooseServiceItem.name}}</div>
+            <div class="des-detail fs24 color-gray" v-html="chooseServiceItem.detail"></div>
         </div>
     </div>
+    <div class="form-con">
+        <!-- <div class="form-box" data-for-des="基础表单提交数据">
+            <div flex class="form-item">
+                <div class="label fs24 color-black">商户名称</div>
+                <div flex>
+                    <input flex class="color-black fs30" type="text" v-model="baseParameter.">
+                </div>
+            </div>
+            <div flex class="form-item">
+                <div class="label fs24 color-black">联系人</div>
+                <div flex>
+                    <input flex class="color-black fs30" type="text" v-model="">
+                </div>
+            </div>
+            <div flex class="form-item">
+                <div class="label fs24 color-black">联系电话</div>
+                <div flex>
+                    <input flex class="color-black fs30" type="text" v-model="tradeItem.merchantName">
+                </div>
+            </div>
+        </div> -->
+        <div class="form-box">
+            <div flex class="form-item p-b-3" data-for-des="适用门店" v-if="formType == 2 || formType == 4">
+                <div class="label fs24 color-black fwb">适用门店</div>
+                <div flex>
+                    <div layout="row" :class="{'color-tiffany-blue' : checkStoreSelect(item)}" layout-align="start center" @click="clickChooseSpecCode(item, 1)" v-for="(item, index) in storeList" :key="index" class="p-t-3 p-b-3 select-li">
+                        <m-icon class="fs36 m-r-2" xlink="#icon-gouicon1"></m-icon>
+                        <span class="fs24">{{item.name}}</span>
+                    </div>
+                </div>
+            </div>
+            <div flex class="form-item p-b-3 no-border" data-for-des="续约时间" v-if="formType == 2 || formType == 4">
+                <!-- <div class="label fs24 color-black">续约时间</div> -->
+                <div flex>
+                    <div layout="row" :class="{'color-tiffany-blue' : item.specCode == baseParameter.specCode}" layout-align="space-between center" @click="clickChooseSpecCode(item, 3)" v-for="(item, index) in contractTime" :key="index" class="p-t-3 p-b-3 select-li">
+                        <span class="fs24">{{item.specName}}</span>
+                        <m-icon class="fs32" v-if="item.specCode == baseParameter.specCode" xlink="#icon-check__"></m-icon>
+                    </div>
+                </div>
+            </div>
+            <div v-for="(item, index) in addStoreData" :key="index" v-if="formType == 3" class="add-store-group p-2 m-b-3">
+                <div flex class="form-item" data-for-des="门店名称">
+                    <div class="label fs24 color-black fwb">门店名称</div>
+                    <div flex>
+                        <input flex class="color-black fs30" type="text" v-model="item.storeName">
+                    </div>
+                </div>
+                <div flex class="form-item" data-for-des="门店地址">
+                    <div class="label fs24 color-black fwb">门店地址</div>
+                    <div flex>
+                        <input flex class="color-black fs30" type="text" v-model="item.storeAddress">
+                    </div>
+                </div>
+                <div flex class="form-item" data-for-des="联系电话">
+                    <div class="label fs24 color-black fwb">联系电话</div>
+                    <div flex>
+                        <input flex class="color-black fs30" type="text" v-model="item.storeContactPhone">
+                    </div>
+                </div>
+                <div flex class="form-item p-b-3 no-border" data-for-des="门店类型">
+                    <div class="label fs24 color-black fwb m-b-2">门店类型</div>
+                    <div flex>
+                        <div layout="row" layout-align="space-between center" @click="clickChooseSpecCode(storeType, 2, item)" v-for="(storeType, index) in relationType" :key="index" class="p-t-3 p-b-3 select-li">
+                            <span class="fs24">{{storeType.name}}</span>
+                            <m-icon class="fs32 color-tiffany-blue" v-if="storeType.value == item.relationType" xlink="#icon-check__"></m-icon>
+                        </div>
+                    </div>
+                </div>
+                <div flex class="text-center" @click="removeStore(index)" v-if="addStoreData.length > 1">
+                    <m-icon class="fs28 color-tiffany-blue" xlink="#icon-shanchu1"></m-icon>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="add-tradeItemSpecList p-4 m-b-3" layout="row" v-if="formType == 3" layout-align="center center" @click="add">
+        <div layout="row" layout-align="center center">
+            增加门店&nbsp;&nbsp;
+            <m-icon class="fs24" xlink="#icon-jia"></m-icon>
+        </div>
+        <div layout="row" layout-align="center center" flex-offset="5">
 
-</div>
-<div class="submit color-pink fs40" layout="row" layout-align="center center" @click="toPay" flex>
-    提交
-</div>
-<m-picker v-model="showStore" :slots="slots" valueKey="name" @confirm="onValuesChange"></m-picker>
+        </div>
+
+    </div>
+    <div class="submit color-pink fs40" layout="row" layout-align="center center" @click="toPay" flex>
+        提交
+    </div>
 </div>
 </template>
 <script>
+/**
+ * 1：系统续费
+ * 2：活动使用权
+ * 3：新增门店
+ * 4：门店续费
+ */
 import Vue from 'vue';
-import { Switch, Indicator, Swipe, SwipeItem } from 'mint-ui';
+import { Indicator, Swipe, SwipeItem } from 'mint-ui';
 import api_party from 'services/api.party';
-import mPicker from 'components/m-picker';
-Vue.component(Switch.name, Switch);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
 export default {
     data() {
         return {
-            date: new Date(),
             value: false,
             employee: JSON.parse(localStorage.getItem('employee')),
-            parameter: {
-                merchantId: this.$store.getters.merchantId,
-                merchantName: this.$store.getters.merchantName,
-                employeeName: ''
-            },
-            showBuy: false,
+            relationType: [
+                {
+                    name: '直营店',
+                    value: '1'
+                },
+                {
+                    name: '加盟店',
+                    value: '2'
+                }
+            ],
             chooseServiceItem: {},
-            store: {},
-            slots: [{
-                flex: 1,
-                values: []
-            }],
-            showStore: false
+            contractTime: [],
+            storeList: [],
+            formShowType: 1,
+            chooseStore: [],
+            addStoreData: [
+                {
+                    storeName: null,
+                    storeAddress: null,
+                    storeContactPhone: null,
+                    relationType: 1,
+                    specCode: null
+                }
+            ],
+            baseParameter: {
+                specCode: null,
+                quantity: null,
+                storeId: null,
+                storeName: null,
+                storeAddress: null,
+                storeContactPhone: null,
+                longitude: null,
+                latitude: null,
+                relationType: null
+            },
+            formType: null
         };
     },
     methods: {
-        chooseDate() {
-            this.$refs.picker.open();
-        },
-        handleConfirm(data) {
-            // this.date = Vue.filter('amDateFormat', [data]);
-        },
         loadStoreList() {
             Indicator.open('loading...');
             api_party.storeList(this.$store.state.party.merchantId, this.$store.getters.employeeId).then(msg=> {
                 Indicator.close();
-                this.slots[0].values = msg.data;
+                this.storeList = msg.data;
+                this.parameter.storeId = this.storeList[0].id;
             }, msg=> {
 
             });
@@ -81,16 +172,10 @@ export default {
             this.loadStoreList();
             this.loadActivityDetail();
         },
-        update(val) {
-            this.showBuy = val;
-        },
-        chooseStore() {
-            this.showStore = true;
-        },
-        onValuesChange(value) {
-            this.store = value[0];
-        },
         toPay() {
+            if (!this.checkForm()) {
+                return;
+            };
             this.$router.push({
                 name: 'pay-detail',
                 params: {
@@ -104,18 +189,102 @@ export default {
             api_party.productDetail(this.$route.params.id).then(msg=> {
                 Indicator.close();
                 this.chooseServiceItem = msg.data;
-                this.chooseServiceItem.images = this.chooseServiceItem.images.split(',');
+                this.contractTime = this.chooseServiceItem.goodsSpecList;
+                this.chooseServiceItem.images = this.chooseServiceItem.images ? this.chooseServiceItem.images.split(',') : this.chooseServiceItem.images;
+                this.formType = this.chooseServiceItem.formType;
+                if (this.formType == 2) {
+                    this.baseParameter.specCode = '1y';
+                }
             }, msg=> {
 
             });
+        },
+        clickChooseSpecCode(item, type, data) {
+            switch (type) {
+                case 1:
+                    let tempIndex;
+                    this.chooseStore.map((store, index)=> {
+                        if (store == item) {
+                            tempIndex = index;
+                        }
+                    });
+                    if (tempIndex || tempIndex == 0) {
+                        this.chooseStore.splice(tempIndex, 1);
+                    } else {
+                        this.chooseStore.push(item);
+                    };
+                    break;
+                case 2:
+                    data.relationType = item.value;
+                    break;
+                case 3:
+                    this.baseParameter.specCode = item.specCode;
+            }
+        },
+        add() {
+            this.addStoreData.push(
+                {
+                    storeName: null,
+                    storeAddress: null,
+                    storeContactPhone: null,
+                    relationType: null,
+                    specCode: null
+                }
+            );
+
+        },
+        removeStore(index) {
+            if (this.addStoreData.length == 1) {
+                this.$messageBox('只有一家门店咯~');
+                return;
+            }
+            this.addStoreData.splice(index, 1);
+        },
+        checkStoreSelect(item) {
+            let ls = this.chooseStore.filter((store, index)=> {
+                return store == item;
+            });
+            return ls.length;
+        },
+        checkForm() {
+            switch (this.formType) {
+                case '1':
+                // 系统续费
+                    return true;
+                case '3':
+                // 新增门店
+                    let result = true;
+                    this.addStoreData.map((item, index)=> {
+                        if (!item.storeName) {
+                            this.$toast('记得填写门店名称哦~');
+                            result = false;
+                        };
+                        if (!item.storeAddress) {
+                            this.$toast('门店地址为必填哦~');
+                            result = false;
+                        };
+                        if (!item.storeContactPhone) {
+                            this.$toast('记得填写联系电话哦~');
+                            result = false;
+                        }
+                        if (!item.relationType) {
+                            this.$toast('请选择门店类型哦~');
+                            result = false;
+                        }
+                    });
+                    return result;
+                case '4':
+                case '2':
+                // 门店续费
+                    break;
+            }
+
         }
     },
     mounted() {
         this.init();
     },
-    components: {
-        mPicker
-    }
+    components: {}
 };
 </script>
 <style lang="less" scoped>
@@ -133,21 +302,12 @@ export default {
             .check{
                 padding: 10px 0;
             }
-            select{
-                width: 100%;
-                display: block;
-                padding: 10px;
-                background: none;
+            .select-li{
+                border-bottom: 1px solid @border-gay;
             }
-            textarea{
-                width: 100%;
-                resize: none;
-                display: block;
-                margin: 10px 0;
-                border:1px solid @border-gay;
-                border-radius: 7px;
-                height: 50px;
-            }
+        }
+        .no-border{
+            border: none;
         }
         .form-item.last{
             border: none;
@@ -180,6 +340,10 @@ export default {
 
         }
     }
+}
+.add-store-group{
+    border: 1px solid @light-gray;
+    border-radius: 5px;
 }
 }
 </style>
