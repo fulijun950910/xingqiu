bbd55dd1f9e03cc88567873d6eb6d5bcd477dabd
fred_zhao <hui.zhao@mei1.com>
2018-08-06 15:58:28 +0800

    fix[openId重新赋值]

--- source/pages/index/pages/main/index.vue
+++ source/pages/index/pages/main/index.vue
@@ -208,8 +208,7 @@
             },
             goMbh() {
                 if (this.$store.state && this.$store.state.party && this.$store.state.party.partyId) {
-                    let openId = this.$store.state.openId;
-                    console.log(this.$store.state);
+                    let openId = JSON.parse(localStorage.getItem('employee')).openId;
                     api_party.bandWeichat(this.$store.state.party.id, openId).then(msg=> {
                         window.location.href = `http://b2b.mei1.info/app/index.php?i=1&c=entry&eid=41&saasUID=${this.$store.state.party.id}`;
                     }, msg=> {
