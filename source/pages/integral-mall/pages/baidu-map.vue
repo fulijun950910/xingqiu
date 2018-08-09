227b86121e92999080c866efcc994f1aa59b7381
fred_zhao <hui.zhao@mei1.com>
2018-08-09 10:04:16 +0800

    解决冲突

---
+++ source/pages/integral-mall/pages/baidu-map.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="baidu-map" id="allmap"></div>
+</template>
+
+<script>
+// import bMap from '../map.js';
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+        loadMap() {
+            // bMap.init()
+        }
+    },
+    mounted() {
+    }
+};
+</script>
+<style lang="less" scoped>
+@import '~styles/_style';
+.baidu-map {
+    height: 100%;
+    width: 100%;
+}
+</style>
+
