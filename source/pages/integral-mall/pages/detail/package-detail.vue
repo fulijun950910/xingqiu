5d0546a4a6a2e50cd7e54f8c1f72a31eec44fbde
fred_zhao <hui.zhao@mei1.com>
2018-08-28 12:02:00 +0800

    fix[美问星球套餐]

--- source/pages/integral-mall/pages/detail/package-detail.vue
+++ source/pages/integral-mall/pages/detail/package-detail.vue
@@ -1,94 +1,116 @@
 <template>
-    <div class="package-detail" v-title="'美豆豆盒'">
-        <div class="c-con p-t-3 p-b-3 p-l-3 p-r-3 bg-color-bg" flex layout="row" layout-align="start center">
-            <div class="no-flex c-box bg-primary m-r-2" layout="column" layout-align="center center" v-for="(item, index) in goodsGroupList" :key="index">
-                    <span class="fs28 color-white">{{item.groupName}}</span>
-                    <span class="fs20 color-white">{{item.select.length}}/{{item.quantity ? item.quantity : '无限'}}</span>
-            </div>
+<div class="package-detail" v-title="'美豆豆盒'">
+    <div class="c-con p-t-3 p-b-3 p-l-3 p-r-3 bg-color-bg" flex layout="row" layout-align="start center">
+        <div class="no-flex c-box bg-primary m-r-2" layout="column" layout-align="center center" v-for="(item, index) in goodsGroupList" :key="index">
+            <span class="fs28 color-white">{{item.groupName}}</span>
+            <span class="fs20 color-white">{{item.select.length}}/{{item.quantity ? item.quantity : '无限'}}</span>
         </div>
-        <div class="bg-color-bg" flex>
-            <div flex  v-for="(item, index) in goodsGroupList" :key="index" class="m-b-3 p-3 sku bg-white" :class="{'active' : checkActive(goodsGroupList[index].select, item), 'toggle' : item.toggle}">
+    </div>
+    <div class="bg-color-bg" flex>
+        <div flex v-for="(item, index) in goodsGroupList" :key="index" class="m-b-3 p-3 sku bg-white" :class="{'active' : checkActive(goodsGroupList[index].select, item), 'toggle' : item.toggle}">
             <div layout="column" layout-align="center center" class="toggle p-t-2 p-b-2" @click="toggle(item, 'toggle', 'icon')">
                 <div flex layout="row" layout-align="center center">
-                <span class="fs28 fwb">-选择{{item.groupName}}-</span>
-               <span class="toggle-icon"><m-icon class="fs40 extra-light-gray" :xlink="item.icon"></m-icon></span>
+                    <span class="fs28 fwb">-选择{{item.groupName}}-</span>
+                    <span class="toggle-icon"><m-icon class="fs40 extra-light-gray" :xlink="item.icon"></m-icon></span>
                 </div>
                 <div class="fs20">{{item.goodsGroupGoodsList.length}}个{{item.groupName}}中选择{{item.quantity}}个</div>
             </div>
             <div class="sku-con border-top" v-if="item.toggle">
                 <div class="m-b-4 p-t-3 border-bottom" :class="{'active' : checkActive(item.select, sku, 1)}" v-for="(sku, skuIndex) in item.goodsGroupGoodsList" :key="skuIndex">
                     <div layout="row" layout-align="start center" class="p-b-3" @click="chooseActivity(sku, item)">
-                    <div class="sku-img">
-                        <img :src="sku.image | nSrc(require('assets/imgs/location.jpg'))" alt="">
-                    </div>
-                    <div flex-offset="5">
-                        <div class="fwb fs24 m-b-2">{{sku.goodsName}}</div>
-                        <div class="fs24">规格：{{sku.specification ? sku.specification.specName : '无'}}</div>
-                    </div>
-                    <div layout="row" flex layout-align="end center" v-if="sku.formType !=4 && sku.formType !=3">
-                        <m-icon class="fs40 steel-gray add m-r-3" xlink="#icon-gouxuanshixin"></m-icon>
-                    </div>
+                        <div class="sku-img">
+                            <img :src="sku.image | nSrc(require('assets/imgs/location.jpg'))" alt="">
+                        </div>
+                        <div flex-offset="5">
+                            <div class="fwb fs24 m-b-2">{{sku.goodsName}}</div>
+                            <div class="fs24">规格：{{sku.specification ? sku.specification.specName : '无'}}</div>
+                        </div>
+                        <div layout="row" flex layout-align="end center" v-if="sku.formType !=4 && sku.formType !=3">
+                            <m-icon class="fs40 steel-gray add m-r-3" xlink="#icon-gouxuanshixin"></m-icon>
+                        </div>
                     </div>
                     <div flex class="border-bottom" name="续费门店" v-if="sku.formType == 4">
-                    <div class="fs24 text-center color-tiffany-blue p-t-2 p-b-2" @click="toggle(sku, 'storeToggle', 'storeIcon')">点击选择续费门店<m-icon :xlink="sku.storeIcon"></m-icon></div>
-                     <div layout="row" layout-align="space-between center" :class="{'border-bottom' : storeIndex != sku.storeList.length - 1}" v-if="sku.storeToggle" class="p-t-3 p-b-3" @click="choose(sku, store, 2, item)" v-for="(store, storeIndex) in sku.storeList" :key="storeIndex">
-                         <div class="fs24">{{store.name}}</div>
-                         <div v-if="checkActive(sku.stores, store, 1)"><m-icon class="color-tiffany-blue" xlink="#icon-check__"></m-icon></div>
-                     </div>
-                    </div>    
-                    <div v-if="sku.formType == 3" flex name="新增门店">
-                        <div class="fs24 text-center color-tiffany-blue p-t-2 p-b-2" @click="toggle(sku, 'addStoreToggle', 'addStoreIcon')">点击填写添加门店<m-icon :xlink="sku.addStoreIcon"></m-icon></div>
-                        <div v-for="(newStore, newStoreIndex) in sku.addStores" :key="newStoreIndex" v-if="sku.addStoreToggle">
-                          <div layout="row" layout-align=" center" class="p-t-2 p-b-2" :class="{'border-bottom' :newStoreIndex!= sku.addStores.length -1 }">
-                              <input flex type="text" placeholder="请填写门店名称" v-model="newStore.storeName">
-                          </div>
-                          <div layout="row" layout-align=" center" class="p-t-2 p-b-2">
-                              <input flex type="text" placeholder="请填联系电话" v-model="newStore.storeName">
-                          </div>
-                          <div layout="row" layout-align=" center" class="p-t-2 p-b-2">
-                              <input flex type="text" placeholder="请填写门店类型" v-model="newStore.storeName">
-                          </div>
-                          <div layout="row" layout-align=" center" class="p-t-2 p-b-2">
-                              <input flex type="text" placeholder="请选择规格" v-model="newStore.storeName">
-                          </div>
+                        <div class="fs24 text-center color-tiffany-blue p-t-2 p-b-2" @click="toggle(sku, 'storeToggle', 'storeIcon')">点击选择续费门店
+                            <m-icon :xlink="sku.storeIcon"></m-icon>
                         </div>
-                             <div @click="addStore(sku, newStoreIndex, 1)">添加门店</div>
-                        </div>                
-                    <div flex v-if="sku.goodsGroupGoodsSpecList.length > 1" class="border-bottom" name="选择规格">
-                        <div class="fs24 text-center color-tiffany-blue p-t-2 p-b-2" @click="toggle(sku, 'toggle', 'icon')" flex>点击选择规格<m-icon :xlink="sku.icon"></m-icon></div>
+                        <div layout="row" layout-align="space-between center" :class="{'border-bottom' : storeIndex != sku.storeList.length - 1}" v-if="sku.storeToggle" class="p-t-3 p-b-3" @click="choose(sku, store, 2, item)" v-for="(store, storeIndex) in sku.storeList" :key="storeIndex">
+                            <div class="fs24">{{store.name}}</div>
+                            <div v-if="checkActive(sku.stores, store, 1)">
+                                <m-icon class="color-tiffany-blue" xlink="#icon-check__"></m-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-if="sku.formType == 3" flex name="新增门店">
+                        <div class="fs24 text-center color-tiffany-blue p-t-2 p-b-2" @click="toggle(sku, 'addStoreToggle', 'addStoreIcon')">点击填写添加门店
+                            <m-icon :xlink="sku.addStoreIcon"></m-icon>
+                        </div>
+                        <div v-for="(newStore, newStoreIndex) in sku.addStores" :key="newStoreIndex" v-if="sku.addStoreToggle" :class="{'border-bottom' :newStoreIndex!= sku.addStores.length -1 }">
+                            <div flex layout="row" layout-align="center center" v-if="newStoreIndex != 0 && priceType == 3" @click="addStore(sku, newStoreIndex, 2)" class="p-t-3 p-b-3 border-bottom">删除&nbsp;&nbsp;<m-icon xlink="#icon-shanchuicon1" class="fs34"></m-icon></div>
+                            <div layout="row" layout-align="start center" class="p-t-2 p-b-2 border-bottom">
+                                <input flex type="text" placeholder="请填写门店名称" v-model="newStore.storeName">
+                            </div>
+                            <div layout="row" layout-align="start center" class="p-t-2 p-b-2 border-bottom">
+                                <input flex type="number" placeholder="请填联系电话" v-model="newStore.storeContactPhone">
+                            </div>
+                            <div layout="row" layout-align="start center" class="p-t-2 p-b-2 border-bottom">
+                                <!-- <input flex type="text" placeholder="请填写门店类型" v-model="newStore.storeName"> -->
+                                <mt-radio title="选择门店类型" v-model="newStore.relationType" class="fs24" :options="newStore.storeType">
+                                </mt-radio>
+                            </div>
+                            <div layout="row" layout-align="start center" class="p-t-2 p-b-2 border-bottom">
+                                <mt-radio title="选择规格" v-model="newStore.specCode" class="fs24" :options="newStore.specList">
+                                </mt-radio>
+                                <!-- <input flex type="text" placeholder="请选择规格" v-model="newStore.storeName"> -->
+                            </div>
+                        </div>
+                        <div flex layout="row" layout-align="center cneter" v-if="priceType == 3">
+                            <div class="add-store fs24 p-t-3 p-b-3" @click="addStore(sku, '', 1)" layout="row" layout-align="center center">添加门店&nbsp;
+                                <m-icon xlink="#icon-xingzhuang1" class="fs24"></m-icon>
+                            </div>
+                        </div>
+                    </div>
+                    <div flex v-if="sku.goodsGroupGoodsSpecList.length > 1 && sku.formType != 3" class="border-bottom" name="选择规格">
+                        <div class="fs24 text-center color-tiffany-blue p-t-2 p-b-2" @click="toggle(sku, 'toggle', 'icon')" flex>点击选择规格
+                            <m-icon :xlink="sku.icon"></m-icon>
+                        </div>
                         <div flex class="p-b-3 border-top" v-if="sku.toggle">
-                            <div class="p-b-3 p-t-3" @click="choose(sku, spec, 1)" :class="{'color-tiffany-blue' : spec.active,'border-bottom' : specIndex != sku.goodsGroupGoodsSpecList.length - 1}" v-for="(spec, specIndex) in sku.goodsGroupGoodsSpecList" :key="specIndex" layout="row" layout-align="space-between center">
-                               <div class="fs24">{{spec.specName}}</div>
-                               <div class="choose-circle color-tiffany-blue" v-if="checkActive(sku.specification, spec, 2)">
-                                   <m-icon xlink="#icon-check__"></m-icon>
-                               </div>
+                            <div class="p-b-3 p-t-3" @click="choose(sku, spec, 1)" :class="{'color-tiffany-blue' : spec.active,'border-bottom' : specIndex != sku.goodsGroupGoodsSpecList.length - 1}" v-for="(spec, specIndex) in sku.goodsGroupGoodsSpecList" :key="specIndex" layout="row"
+                                layout-align="space-between center">
+                                <div class="fs24">{{spec.specName}}</div>
+                                <div class="choose-circle color-tiffany-blue" v-if="checkActive(sku.specification, spec, 2)">
+                                    <m-icon xlink="#icon-check__"></m-icon>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            </div>
-        </div>
-        <div class="submit-btn border-top" layout="row" layout-align="start stretch">
-            <div flex="70" layout="row" layout-align="start center">
-                <div flex-offset="5" class="color-pink">￥<span class="fs34">{{price | fen2yuan}}</span></div>
-            </div>
-            <button flex="30" layout="row" class="fs28" @click="toPay" layout-align="center center" :disabled="checkSubmit() ? true : false">
-                {{!checkSubmit() ? '提交' : '尚未选满'}}
-            </button>
         </div>
     </div>
+    <div class="submit-btn border-top" layout="row" layout-align="start stretch">
+        <div flex="70" layout="row" layout-align="start center">
+            <div flex-offset="5" class="color-pink">￥<span class="fs34">{{price | fen2yuan}}</span></div>
+        </div>
+        <button flex="30" layout="row" class="fs28" @click="toPay" layout-align="center center" :disabled="checkSubmit() ? true : false">
+                {{!checkSubmit() ? '提交' : '尚未选满'}}
+            </button>
+    </div>
+</div>
 </template>
 
 <script>
+import Vue from 'vue';
 import api_party from 'services/api.party';
+import { Radio } from 'mint-ui';
+Vue.component(Radio.name, Radio);
 export default {
     data() {
         return {
             productId: this.$route.params.id,
             tradeGoodsGroupList: [],
             goodsGroupList: [],
-            price: 0
+            price: 0,
+            priceType: 2
         };
     },
     methods: {
@@ -96,12 +118,6 @@ export default {
             this.$indicator.open('loading...');
             api_party.storeList(this.$store.state.party.merchantId, this.$store.getters.employeeId).then(msg=> {
                 this.$indicator.close();
-                // if (msg.data.length > 1) {
-                //     msg.data.unshift({
-                //         name: '全选',
-                //         id: 'all'
-                //     });
-                // };
                 this.productDetail(msg.data);
             }, msg=> {
 
@@ -112,6 +128,7 @@ export default {
             api_party.productDetail(this.productId).then(msg=> {
                 this.$indicator.close();
                 this.price = msg.data.priceType == 2 ? msg.data.price : 0;
+                this.priceType = msg.data.priceType;
                 if (msg.data && msg.data.goodsGroupList && msg.data.goodsGroupList.length) {
                     this.goodsGroupList = JSON.parse(JSON.stringify(msg.data.goodsGroupList));
                     this.goodsGroupList.map((item, index)=> {
@@ -130,19 +147,54 @@ export default {
                                 this.$set(sku, 'storeToggle', false);
                                 this.$set(sku, 'toggle', false);
                                 this.$set(sku, 'active', false);
+                                this.$set(sku, 'specCode', sku.goodsGroupGoodsSpecList[0].code);
                                 this.$set(sku, 'storeList', data);
-                                this.$set(sku, 'addStores', [{
+                                let tempStoreCon = [];
+                                let storeData = {
                                     storeName: '',
                                     storeContactPhone: '',
-                                    relationType: ''
-                                }]);
-                                if (data.length == 1) {
-                                    this.$set(sku, 'stores', [data[0]]);
-                                }
+                                    relationType: '1',
+                                    specCode: sku.goodsGroupGoodsSpecList[0].specCode,
+                                    storeType: [
+                                        {
+                                            label: '直营店',
+                                            value: '1'
+                                        },
+                                        {
+                                            label: '加盟店',
+                                            value: '2'
+                                        }
+                                    ],
+                                    specList: []
+                                };
+                                sku.goodsGroupGoodsSpecList.forEach(element=> {
+                                    storeData.specList.push({
+                                        label: element.specName,
+                                        value: element.specCode
+                                    });
+                                });
+                                tempStoreCon = [storeData];
+                                if (msg.data.priceType == 2) {
+                                    tempStoreCon = [];
+                                    for (let i = 0; i < item.quantity; i++) {
+                                        tempStoreCon.push(storeData);
+                                    }
+                                };
+                                if (sku.formType == 3) {
+                                    this.$set(sku, 'addStores', tempStoreCon);
+                                    // this.$set(sku, 'select', tempStoreCon);
+                                };
+                                if (sku.formType == 4) {
+                                    // 传入进来的门店初始化
+                                    if (data.length == 1) {
+                                        this.$set(sku, 'stores', [data[0]]);
+                                    }
+                                };
                             });
                         };
                     });
                 };
+                console.log(this.goodsGroupList);
             }, msg=> {
             });
         },
@@ -255,13 +307,23 @@ export default {
         addStore(obj, index, type) {
             if (type == 1) {
                 // 添加门店
-                obj.push({
+                obj.addStores.push({
                     storeName: '',
                     storeContactPhone: '',
-                    relationType: ''
+                    relationType: '',
+                    storeType: [
+                        {
+                            label: '直营店',
+                            value: '1'
+                        },
+                        {
+                            label: '加盟店',
+                            value: '2'
+                        }
+                    ]
                 });
             } else if (type == 2) {
-                obj.splice(index, 1);
+                obj.addStores.splice(index, 1);
             }
         },
         toPay() {
@@ -293,7 +355,6 @@ export default {
                 });
                 this.tradeGoodsGroupList.push(temp);
             });
-            console.log(this.tradeGoodsGroupList);
             this.$router.push({
                 name: 'pay-detail',
                 params: {
@@ -320,6 +381,12 @@ export default {
     overflow: hidden;
     padding-top:115px;
     padding-bottom: 50px;
+    .mint-cell{
+        label{
+        font-size: @fs24;
+
+        }
+    }
     .no-flex{
         flex: none;
     }
@@ -400,6 +467,11 @@ export default {
     button[disabled="disabled"] {
             background: @extra-light-gray;
             color: @gray;
+    }
+    .add-store{
+        width: 100px;
+        border-radius: 10px;
+        margin: 0 auto;
     }
 
 }
