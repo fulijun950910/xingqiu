031c4a7c484f0a410cedd98c8ff781081a27cdba
yinxianwei <yinxianwei@qq.com>
2018-09-04 15:55:35 +0800

    config: vue-cli3.x

---
+++ app.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    'service-subtotal': {
+        entry: 'source/pages/service-subtotal',
+        title: '服务小计',
+        cdn: {
+            js: [],
+            css: []
+        }
+    },
+    'customer-profiles': {
+        entry: 'source/pages/customer-profiles',
+        title: '顾客档案',
+        cdn: {
+            js: [],
+            css: []
+        }
+    },
+    'shop': {
+        entry: 'source/pages/shop',
+        title: '美博汇',
+        cdn: {
+            js: [],
+            css: []
+        }
+    },
+    'index': {
+        entry: 'source/pages/index',
+        title: '',
+        cdn: {
+            js: [],
+            css: []
+        }
+    },
+    'integral-mall': {
+        entry: 'source/pages/integral-mall',
+        title: '',
+        cdn: {
+            js: [],
+            css: []
+        }
+    },
+};
