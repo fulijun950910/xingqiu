26459d41154dbd4931c4eb0cf6fbddd9ebd9d501
yinxianwei <yinxianwei@qq.com>
2018-09-30 14:13:24 +0800

    修改环境配置

--- config/dev/app.config.js
+++ config/dev/app.config.js
@@ -5,6 +5,6 @@
 //app.url = 'http://192.168.27.161:8080';
 //app.url = 'http://192.168.27.199:8069';
 //app.url = 'http://192.168.27.218:8080';
-app.url = 'https://assistant.mei1.info';
+app.url = '';
 app.urlC = 'https://wechat.mei1.info';
-app.filePath = 'http://localhost:8080/api/file/';
+app.filePath = '/api/file/';
