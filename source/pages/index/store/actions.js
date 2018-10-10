daf34a776ecade7235848c15c1b59a24fd6ee989
yinxianwei <yinxianwei@qq.com>
2018-10-10 14:06:38 +0800

    fix: 修复权限刷新后不更新的问题

--- source/pages/index/store/actions.js
+++ source/pages/index/store/actions.js
@@ -1,19 +1,21 @@
 import apiSignIn from 'services/api.signIn';
 export default {
-    async updateBaseData(context) {
-        if (context.getters.merchantId) {
-            Promise.all([apiSignIn.getPermission(context.getters.employeeId), apiSignIn.getNounConfig(context.getters.merchantId)]).then(
-                res => {
-                    context.commit('updatePermissionPackage', res[0].data);
-                    context.commit('updateNounConfig', res[1].data);
-                    return true;
-                },
-                err => {
-                    return Promise.reject(err);
-                }
-            );
-        } else {
-            return Promise.reject();
-        }
+    updateBaseData(context) {
+        return new Promise((resolve, reject) => {
+            if (context.getters.merchantId) {
+                Promise.all([apiSignIn.getPermission(context.getters.employeeId), apiSignIn.getNounConfig(context.getters.merchantId)]).then(
+                    res => {
+                        context.commit('updatePermissionPackage', res[0].data);
+                        context.commit('updateNounConfig', res[1].data);
+                        resolve();
+                    },
+                    err => {
+                        reject(err);
+                    }
+                );
+            } else {
+                reject();
+            }
+        });
     }
 };
