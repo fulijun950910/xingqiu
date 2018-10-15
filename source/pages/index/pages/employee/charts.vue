4187332201fdb817736e32c03a2346f2f6888734
yinxianwei <yinxianwei@qq.com>
2018-10-15 16:50:40 +0800

    add: 员工业绩卡耗页面

---
+++ source/pages/index/pages/employee/charts.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+    </div>
+</template>
+<script>
+export default {
+    name: 'employee-charts',
+    mixins: [],
+    props: {},
+    components: {},
+    computed: {},
+    data() {
+        return {};
+    },
+    mounted() {},
+    methods: {}
+};
+</script>
+<style lang="less">
+@import '~@/styles/_agile';
+</style>
