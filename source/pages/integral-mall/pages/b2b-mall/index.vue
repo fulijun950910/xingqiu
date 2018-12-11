fc497852ac368501887928cf2ee42ac4d43ea6e7
pan.xu <pan.xu@mei1.com>
2018-12-11 14:45:45 +0800

    fix:b2b发票

--- source/pages/integral-mall/pages/b2b-mall/index.vue
+++ source/pages/integral-mall/pages/b2b-mall/index.vue
@@ -1,22 +1,29 @@
 <template>
     <div class='b2b-mall-index'>
         <div class="banner1-box">
-            <img :src="null | mSrc2(require('assets/imgs/nullimg.jpg'))" alt="">
+            <div class="swipe-box">
+                <mt-swipe :auto="4000">
+                    <mt-swipe-item v-for="item in indexData.topBanner" :key="item.id">
+                        <img :src="item.imageId | mSrc2(require('assets/imgs/nullimg.jpg'))" alt="">
+                        <!--<img :src="6609984741633299 | mSrc2(require('assets/imgs/nullimg.jpg'))" alt="">-->
+                    </mt-swipe-item>
+                </mt-swipe>
+            </div>
         </div>
         <div class='cell cell-box bg-white border-bottom' layout='row' layout-align='start center'>
             <div flex class='fs32 fwb'>经营数据</div>
         </div>
         <div class="bg-white p-t-2 p-b-2 text-center" layout="row">
             <div class="p-t-1 p-b-1" flex="33">
-                <div class="fwb fs48">45</div>
+                <div class="fwb fs40">{{indexData.yesterdayIncome|fen2yuan}}</div>
                 <div class="extra-light-black">昨日收入</div>
             </div>
             <div class="p-t-1 p-b-1 border-left" flex="33">
-                <div class="fwb fs48">45</div>
+                <div class="fwb fs40">{{indexData.unVerifyTicketCount}}</div>
                 <div class="extra-light-black">待验券</div>
             </div>
             <div class="p-t-1 p-b-1 border-left" flex="33">
-                <div class="fwb fs48">45</div>
+                <div class="fwb fs40">{{indexData.waitSuccessGroupCount}}</div>
                 <div class="extra-light-black">待成团</div>
             </div>
         </div>
@@ -32,8 +39,8 @@
             <div class="extra-light-black fs24">占位文案占位文案占位文案占位文案</div>
         </div>
 
-        <div class="m-t-3 banner2-box">
-            <img :src="require('assets/imgs/b2b-mall/2018120601.jpg')" alt="">
+        <div @click="goBannerUrl" class="m-t-3 banner2-box">
+            <img :src="indexData.midBanner.imageId | mSrc2(require('assets/imgs/nullimg.jpg'))" alt="">
         </div>
         <!--商品管理-->
         <div class='m-t-3 cell cell-box bg-white border-bottom' layout='row' layout-align='start center'>
@@ -76,9 +83,13 @@
 <script>
 import Vue from 'vue';
 import {
+    Swipe,
+    SwipeItem,
     InfiniteScroll
 } from 'mint-ui';
 
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 Vue.use(InfiniteScroll);
 import api_b2bmall from 'services/api.b2bmall';
 import navBar from './modules/nav-bar';
@@ -88,6 +99,9 @@ export default {
     name: 'index',
     data() {
         return {
+            indexData: {
+                midBanner: {}
+            },
             mallList: [],
             loading: false,
             isLoadOver: false, // false已加载完所有数据
@@ -103,9 +117,17 @@ export default {
     },
     mounted() {
         this.loadData();
+        this.init();
     },
     methods: {
-        init() {
+        async init() {
+            let data = {
+                merchantId: this.$store.state.party.merchantId
+            };
+            this.$indicator.open();
+            let res = await api_b2bmall.getPurchaseMerchantIndexInfo(data);
+            this.$indicator.close();
+            this.indexData = res.data;
         },
         loadMore() {
             if (this.isLoadOver) {
@@ -143,6 +165,9 @@ export default {
                     id: item.id
                 }
             });
+        },
+        goBannerUrl() {
+            window.location.href = this.indexData.midBanner.url;
         }
     }
 };
@@ -156,11 +181,20 @@ export default {
     min-height: 100vh;
     background: @bg-gray;
     .banner1-box{
-        height: 90px;
-        img{
-            width:100%;
-            height:100%;
+        padding: 10px;
+        .swipe-box{
+            height: 90px;
+            width: 355px;
+            padding: 0;
+            position:relative;
+            border-radius: 4px;
+            overflow: hidden;
+            img {
+                width: 100%;
+                height: 100%;
+            }
         }
+
     }
     .banner2-box{
         height: 64px;
